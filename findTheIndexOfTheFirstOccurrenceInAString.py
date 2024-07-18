95050e0555b313563d6ab60070872c6c67030925
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-07-18 09:34:27 +0530

    Problem: Find The Index Of The First Occurrence In A String

---
+++ findTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,17 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            return haystack.index(needle)
+        except:
+            return-1
+    
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if not needle:
+#             return 0
+
+#         for i in range(len(haystack) + 1 - len(needle)): # last idx must be a window len of needle
+#             if haystack[i : i + len(needle)] == needle:
+#                 return i
+
+#         return -1
