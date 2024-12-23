5c1bc6e7ebb634986d6d70caedf6f372d581e0f9
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2024-12-23 12:32:34 +0530

    Update Problem: Max Consecutive Ones 3

--- leetcode_75/maxConsecutiveOnes3.py
+++ leetcode_75/maxConsecutiveOnes3.py
@@ -4,14 +4,23 @@ class Solution:
         for right, curr in enumerate(nums):
             if curr: temp += 1
             else:
-                nums[right]=-1
                 temp+=1
                 k-=1    
             while k<0:
-                if nums[left]==-1:
-                    nums[left] = 0
+                if not nums[left]:
                     k+=1
                 temp-=1
                 left+=1
             if temp>ans: ans = temp
         return ans
+
+        # faster version
+        # l = r = 0
+        # for num in nums:
+        #     if num == 0:
+        #         k -= 1
+        #     if k < 0:
+        #         if nums[l] == 0:
+        #             k += 1
+        #         l += 1
+        # return len(nums) - l
