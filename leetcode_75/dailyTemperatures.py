e8c51a822875fda50fbc825753a203daa02f0d8b
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2025-01-14 17:28:23 +0530

    Problem: Daily Temperatures, Online Stock Span

---
+++ leetcode_75/dailyTemperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [0]
+        res = [0]*len(temperatures)
+        for i,temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]]<temp:
+                j = stack.pop()
+                res[j]=i-j
+            stack.append(i)
+        return res
