f539380bdf24a36845dd439ef16e26fc36fc2ba6
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2025-07-01 10:04:01 +0530

    Problem: Find The Original Typed String

--- contest/findTheOriginalTypedString.py
+++ contest/findTheOriginalTypedString.py
@@ -1,15 +1,25 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        # Copied from contest result
-        runs = []
+        ans = 0
         prev = word[0]
         cnt = 1
+        
         for c in word[1:]:
             if c == prev:
                 cnt+=1
             else:
-                runs.append(cnt)
+                ans += cnt-1
                 prev=c
                 cnt=1
-        runs.append(cnt)
-        return 1 + sum(r-1 for r in runs)
+        
+        ans += cnt-1
+        return 1+ans
+    
+        # from editorial
+        # ans = 1
+
+        # for i in range(1, len(word)):
+        #     if word[i-1] == word[i]:
+        #         ans += 1
+        
+        # return ans
