4ef0878d7ff7f07d76149c6bf0997d1dc77123d2
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2025-08-04 10:04:34 +0530

    Problem: Fruit Into Baskets

---
+++ daily/fruitIntoBaskets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        ans = 0
+        left = 0
+        cnt = defaultdict(int)
+
+        for right, fruit in enumerate(fruits):
+            cnt[fruit] += 1
+
+            while len(cnt)>2:
+                cnt[fruits[left]] -= 1
+                if cnt[fruits[left]] == 0:
+                    del cnt[fruits[left]]
+                left += 1
+            
+            ans = max(ans, right-left+1)
+        
+        return ans
