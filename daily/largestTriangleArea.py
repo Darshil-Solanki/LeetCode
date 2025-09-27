18de9a993d569c3324d2a3ea61c0ae7b50a075f1
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2025-09-27 13:21:38 +0530

    Problem: Largest Triangle Area

---
+++ daily/largestTriangleArea.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        ans = 0
+        points.sort()
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i+1, len(points)):
+                x2, y2 = points[j]
+                for k in range(j+1, len(points)):
+                    x3, y3= points[k]
+                    ans = max(0.5*abs(x1*y2+x2*y3+x3*y1 -x2*y1-x3*y2-x1*y3), ans)
+        return ans
