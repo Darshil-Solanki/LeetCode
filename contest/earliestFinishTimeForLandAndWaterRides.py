8058e50b0181c70a9ae11a6044a5baa3ea1d8983
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2026-06-02 09:38:43 +0530

    Problem: Earliest Finish Time For Land And Water Rides

--- contest/earliestFinishTimeForLandAndWaterRides.py
+++ contest/earliestFinishTimeForLandAndWaterRides.py
@@ -1,24 +1,39 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        landRides = list(zip(landStartTime, landDuration))
-        waterRides = list(zip(waterStartTime, waterDuration))
+        def solve(start1, duration1, start2, duration2):
+            finish1 = float("inf")
+            for s, d in zip(start1, duration1):
+                finish1 = min(finish1, s+d)
+            finish2 =  float("inf")
+            for s, d in zip(start2, duration2):
+                finish2 = min(finish2, max(s, finish1)+d)
+            return finish2
 
-        ans = float("inf")
-        for i, l_ride in enumerate(landRides):
-            for j, w_ride in enumerate(waterRides):
-                time1 = time2 = 0
-                time1 = sum(l_ride)
-                if time1<w_ride[0]:
-                    time1 = sum(w_ride)
-                else:
-                    time1 += w_ride[1]
+        land_water = solve(landStartTime, landDuration, waterStartTime, waterDuration)
+        water_land = solve(waterStartTime, waterDuration, landStartTime, landDuration)
 
-                time2 = sum(w_ride)
-                if time2<l_ride[0]:
-                    time2 = sum(l_ride)
-                else:
-                    time2 += l_ride[1]
+        return min(land_water, water_land)
+            
+
+        # landRides = list(zip(landStartTime, landDuration))
+        # waterRides = list(zip(waterStartTime, waterDuration))
+
+        # ans = float("inf")
+        # for i, l_ride in enumerate(landRides):
+        #     for j, w_ride in enumerate(waterRides):
+        #         time1 = time2 = 0
+        #         time1 = sum(l_ride)
+        #         if time1<w_ride[0]:
+        #             time1 = sum(w_ride)
+        #         else:
+        #             time1 += w_ride[1]
+
+        #         time2 = sum(w_ride)
+        #         if time2<l_ride[0]:
+        #             time2 = sum(l_ride)
+        #         else:
+        #             time2 += l_ride[1]
                     
-                ans = min(ans, time1, time2)
+        #         ans = min(ans, time1, time2)
 
-        return ans
+        # return ans
