9bde50d5f603d7e4c89baeda19d9e992031139eb
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2026-07-24 21:35:47 +0530

    Problem: Number Of Unique XOR Triplets 2

--- contest/numberOfUniqueXorTriplets2.py
+++ contest/numberOfUniqueXorTriplets2.py
@@ -1,7 +1,7 @@
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==1 or n==2: return n
+        if n==1: return 1
         uniqueXor = set()
         for i in range(n):
             for j in range(i+1, n):
@@ -11,5 +11,5 @@ class Solution:
         for x in uniqueXor:
             for num in nums:
                 tripletXor.add(num^x)
-        
+
         return len(tripletXor)
