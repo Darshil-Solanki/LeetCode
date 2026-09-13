17dcad4b17e10a03cc61649bb9383bc909f65d9c
Darshil Solanki <119782708+Darshil-Solanki@users.noreply.github.com>
2026-09-13 21:30:40 +0530

    Problem: Image Overlap

---
+++ daily/imageOverlap.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        
+        ones1 = [(r, c) for r in range(n) for c in range(n) if img1[r][c]]
+        ones2 = [(r, c) for r in range(n) for c in range(n) if img2[r][c]]
+
+        cnt = defaultdict(int)
+        ans = 0
+        
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                dr = r1 - r2
+                dc = c1 - c2
+                cnt[(dr, dc)] += 1
+                ans = max(ans, cnt[(dr, dc)])
+        
+        return ans
